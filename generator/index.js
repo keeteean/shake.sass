ad5f322be3c5fbcbbdf90127f09e53a90aa0c910
Krisztian Pinter <krisz@keeteean.com>
2019-04-07 17:43:24 +0200

    Removed yeoman generator.

--- generator/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Generator = require('yeoman-generator');
-
-module.exports = class extends Generator {
-  init() {
-    this.fs.copy(`${__dirname}/templates/**`, this.destinationPath());
-  }
-};
